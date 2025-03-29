eaeefeabb520d8eee999e5a9b4936eebae090ab2
ASimler <adelesimler96@gmail.com>
2025-03-29 18:33:14 +0100

    tp cpt rebours done

--- TP-CompteRebours/index.js
+++ TP-CompteRebours/index.js
@@ -1,77 +1,56 @@
-// Créer la logique de compte à rebours
-
-// choice.value = 5 minutes
-
-// choice.value * 60 = totalSeconds
-
-// let minutes = Math.floor(totalSeconds / 60);
-// let seconds = totalSeconds % 60
-
-// Créer un événement à la validation du form pour lancer le compte à rebours
-
-// input choice : id="choice"
-
-// choice.value = 5;
-console.log(choice.value);
+let timer;
+let totalSeconds;
+let minutes;
+let seconds;
 
 choice.addEventListener('input', (e) => {
-        // Calcul du total en secondes du temps choisi :
-        totalSeconds = choice.value * 60;
-        console.log(totalSeconds);
-    
-        // Ici on fait un Math.floor car lors du compte à rebours il faut que les minutes descendent... pas très clair mais bon :
-        minutes = choice.value;
-        console.log(minutes);
-    
-        // seconds = "00";
-    
-        countdownDisplay.innerHTML = `${minutes} : 00`;
+        if (isNaN(choice.value)) {
+            alert('Veuillez entrer un chiffre');
+        } else {
+            // Calcul du total en secondes du temps choisi :
+            totalSeconds = choice.value * 60;
 
+            minutes = choice.value;
+        
+            countdownDisplay.textContent = `${minutes} : 00`;
+        }
 })
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault(); 
+    // choice.value = "";
+    choice.style.display = "none";
+    // On aurait aussi pu clearInterval(timer); ici plutôt que de disable le form durant le chrono : le user aurait pu alors lancer directement un autre chrono même si l'autre n'était pas terminé. Au choix.
 
-    let timer = setInterval(() => {
-        if (totalSeconds < 1) {
-            console.log('test');
-            countdownDisplay.innerHTML = `0 : 00`;
-            clearInterval(timer);
-            window.location.reload();
-
-        } else {
-            totalSeconds = totalSeconds - 1;
+    timer = setInterval(() => {
+        if (totalSeconds >= 1) {
+            totalSeconds --;
             minutes = Math.floor(totalSeconds / 60);
             seconds = (totalSeconds % 60);
-    
-            if (seconds >= 10) {
-                countdownDisplay.innerHTML = `${minutes} : ${seconds}`;
-            } else {
-                countdownDisplay.innerHTML = `${minutes} : 0${seconds}`;
-            }
+
+            let sec = seconds < 10 ? "0" + seconds : seconds;
+
+            countdownDisplay.textContent = `${minutes} : ${sec}`;
 
             stopTimer.innerHTML = '<button id="btnStopTimer">STOP</button>';
             document.getElementById('btnStopTimer').addEventListener('click', () => {
                 window.location.reload();
-            })
+            });
+
+
+        } else {
+            countdownDisplay.textContent = `C'est terminé !`;
+            clearInterval(timer);
+
+            stopTimer.innerHTML = '<button id="btnStopTimer">Lancer un autre chrono</button>';
+            document.getElementById('btnStopTimer').addEventListener('click', () => {
+                window.location.reload();
+            });
         }
 
-    }, 1000);
+    }, 100);
 
 
     
     });
-
-
-
-
-
-
-        // if (seconds < 10) {
-        //     countdownDisplay.innerHTML = `${minutes} : 0${seconds}`;
-        // } else if (minutes <= 0 && seconds <= 0) {
-        //     countdownDisplay.innerHTML = "0 : 00";
-        // } else {
-        //     countdownDisplay.innerHTML = `${minutes} : ${seconds}`;
-        // }
